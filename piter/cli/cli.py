951e41a364aef97d7c3f5c249b3610e50a9a6f9b
m4nh <degregorio.daniele@gmail.com>
2025-02-10 13:17:01 +0100

    refactoring Agent

--- piter/cli/cli.py
+++ piter/cli/cli.py
@@ -7,13 +7,19 @@ context_settings = {"allow_extra_args": True, "ignore_unknown_options": False}
 
 
 @piter.command("check", context_settings=context_settings)
-def check(name: str = typer.Option(..., help="Name of the person to greet")) -> None:
+def check(
+    name: str = typer.Option(..., help="The name of the person to greet")
+) -> None:
     print(f"Hello {name}")
 
 
 @piter.command("image2base64", context_settings=context_settings)
 def image2base64(
-    image_path: str = typer.Option(..., "-i", help="Input image path")
+    image_path: str = typer.Option(
+        ...,
+        "-i",
+        help="The path to the input image file to be converted to a base64 string",
+    )
 ) -> str:
     import rich
     from piter.utils.images import image_file_to_base64_url
@@ -21,15 +27,46 @@ def image2base64(
     rich.print(image_file_to_base64_url(image_path))
 
 
+def _is_valid_image(item):
+    import pipelime.items as pli
+
+    return isinstance(item, (pli.JpegImageItem, pli.BmpImageItem, pli.PngImageItem))
+
+
+def _process_metadata_value(value):
+    if isinstance(value, float):
+        return f"{value:.4f}"
+    return str(value)
+
+
+def _purge_metadata(item):
+    return {key: _process_metadata_value(value) for key, value in item.items()}
+
+
 @piter.command("images_table_simple", context_settings=context_settings)
 def images_table_simple(
-    title: str = typer.Option("Images Table", help="Title of the HTML file"),
-    folder: Path = typer.Option(..., help="Input Underfolder path"),
-    keys: t.List[str] = typer.Option([], help="List of image keys"),
-    mkeys: t.List[str] = typer.Option([], help="List of metadata keys"),
-    embed: bool = typer.Option(False, help="Embed images in the HTML file"),
-    embed_quality: int = typer.Option(50, help="Embed quality"),
-    output_file: str = typer.Option("", help="Output HTML file"),
+    title: str = typer.Option(
+        "Images Table", help="The title of the generated HTML file"
+    ),
+    folder: Path = typer.Option(
+        ..., help="The path to the folder containing the images"
+    ),
+    keys: t.List[str] = typer.Option(
+        [], help="A list of keys to identify images in the dataset"
+    ),
+    mkeys: t.List[str] = typer.Option(
+        [], help="A list of keys to identify metadata in the dataset"
+    ),
+    embed: bool = typer.Option(
+        False, help="Whether to embed images directly in the HTML file"
+    ),
+    embed_quality: int = typer.Option(
+        50, help="The quality of embedded images (0-100)"
+    ),
+    output_file: str = typer.Option(
+        "",
+        help="The path to save the generated HTML file. If not provided, a temporary file will be created",
+    ),
 ) -> None:
     from piter.renderers.html import ImagesTableSimpleParams, ImagesTableSimple
     from piter.utils.images import numpy_to_base64_url, image_file_to_base64_url
@@ -45,56 +82,36 @@ def images_table_simple(
         print("No images found in the folder")
         return
 
-    if len(keys) > 0:
-        stage = pst.StageKeysFilter(key_list=keys + mkeys)
-        dataset = dataset.map(stage)
-    else:
-        keys = list(sorted(dataset[0].keys()))
+    # If no keys provided, use all keys from first sample
+    if not keys:
+        keys = sorted(dataset[0].keys())
+    elif mkeys:  # Only filter if either keys or mkeys are provided
+        dataset = dataset.map(pst.StageKeysFilter(key_list=keys + mkeys))
 
     def get_image_url(item: pli.ImageItem):
         if not embed:
             return str(item.local_sources[0])
-        else:
-            # return numpy_to_base64_url(item(), quality=embed_quality)
-            return image_file_to_base64_url(item.local_sources[0], embed_quality)
+        return image_file_to_base64_url(item.local_sources[0], embed_quality)
 
-    def is_valid_image(item):
-        return (
-            isinstance(item, pli.JpegImageItem)
-            or isinstance(item, pli.BmpImageItem)
-            or isinstance(item, pli.PngImageItem)
+    # Process samples more efficiently using list comprehensions
+    processed_data = [
+        (
+            {
+                key: get_image_url(sample[key])
+                for key in keys
+                if _is_valid_image(sample[key])
+            },
+            {
+                mkey: _purge_metadata(sample[mkey]())
+                for mkey in mkeys
+                if isinstance(sample[mkey], pli.MetadataItem)
+            },
         )
+        for sample in track(dataset, total=len(dataset), description="Processing")
+    ]
 
-    def is_valid_metadata(item):
-        return isinstance(item, pli.MetadataItem)
-
-    def purge_metadata(item):
-        output_metadata = {}
-        for key, value in item.items():
-            if isinstance(value, float):
-                output_metadata[key] = f"{value:.4f}"
-            else:
-                output_metadata[key] = str(value)
-
-        return output_metadata
-
-    batches = []
-    mbatches = []
-    for sample in track(dataset, total=len(dataset), description="Processing"):
-
-        # Add images in the batch
-        batch = {}
-        for key in keys:
-            if is_valid_image(sample[key]):
-                batch[key] = get_image_url(sample[key])
-        batches.append(batch)
-
-        # Add metadatas in the batch
-        mbatch = {}
-        for mkey in mkeys:
-            if is_valid_metadata(sample[mkey]):
-                mbatch[mkey] = purge_metadata(sample[mkey]())
-        mbatches.append(mbatch)
+    # Unzip the processed data
+    batches, mbatches = map(list, zip(*processed_data)) if processed_data else ([], [])
 
     renderer = ImagesTableSimple()
     output = renderer.render(
@@ -119,16 +136,33 @@ def images_table_simple(
 
 @piter.command("images_clusters_simple", context_settings=context_settings)
 def images_clusters_simple(
-    title: str = typer.Option("Images Clusters", help="Title of the HTML file"),
-    folder: Path = typer.Option(..., help="Input Underfolder path"),
-    image_key: str = typer.Option("image", help="Image key"),
-    label_key: str = typer.Option(
-        "metadata.label", help="Label key [dot notation for nested keys]"
+    title: str = typer.Option(
+        "Images Clusters", help="The title of the generated HTML file"
     ),
-    color_key: str = typer.Option("", help="Color key"),
-    embed: bool = typer.Option(False, help="Embed images in the HTML file"),
-    embed_quality: int = typer.Option(50, help="Embed quality"),
-    output_file: str = typer.Option("", help="Output HTML file"),
+    folder: Path = typer.Option(
+        ..., help="The path to the folder containing the images"
+    ),
+    image_key: str = typer.Option(
+        "image", help="The key to identify images in the dataset"
+    ),
+    label_key: str = typer.Option(
+        "metadata.label",
+        help="The key to identify labels in the dataset (use dot notation for nested keys)",
+    ),
+    color_key: str = typer.Option(
+        "",
+        help="The key to identify colors in the dataset (use dot notation for nested keys)",
+    ),
+    embed: bool = typer.Option(
+        False, help="Whether to embed images directly in the HTML file"
+    ),
+    embed_quality: int = typer.Option(
+        50, help="The quality of embedded images (0-100)"
+    ),
+    output_file: str = typer.Option(
+        "",
+        help="The path to save the generated HTML file. If not provided, a temporary file will be created",
+    ),
 ) -> None:
     from piter.renderers.html import ImagesClustersSimpleParams, ImagesClustersSimple
     from piter.utils.images import numpy_to_base64_url, label_to_color, color_rgb_to_hex
@@ -140,7 +174,6 @@ def images_clusters_simple(
 
     dataset = pls.SamplesSequence.from_underfolder(folder)
 
-    # Check if the label key is nested or not
     is_nested_label = "." in label_key
     label_item = label_key.split(".")[0]
     label_subitem = ".".join(label_key.split(".")[1:]) if is_nested_label else None
@@ -188,7 +221,6 @@ def images_clusters_simple(
             else:
                 colors[label] = label_to_color(label, format="hex")
 
-        # assert label is valid number
         try:
             label = int(label)
         except:
